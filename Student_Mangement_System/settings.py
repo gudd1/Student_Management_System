31edbefae49501eb402d33bce326129ec1719313
gudd1 <83784644+gudd1@users.noreply.github.com>
2023-06-18 14:17:57 +0530

    Update settings.py

--- Student_Mangement_System/settings.py
+++ Student_Mangement_System/settings.py
@@ -132,10 +132,10 @@ STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 '''STATICFILES_DIRS=(
     os.path.join(BASE_DIR, 'static'),
 )'''
-STATICFILES_DIRS = (
+'''STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'Student_Management_Sytem_app/static'),
 
-)
+)'''
 
 '''MIDDLEWARE_CLASSES = (
     'whitenoise.middleware.WhiteNoiseMiddleware',
