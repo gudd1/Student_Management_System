06801fbbbb057cfdccf8fc23b8014083f67fb7c5
gudd1 <83784644+gudd1@users.noreply.github.com>
2023-06-18 14:16:43 +0530

    Update settings.py

--- Student_Mangement_System/settings.py
+++ Student_Mangement_System/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'Student_Mangement_System.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''import dj_database_url
+import dj_database_url
 db_from_env=dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)'''
 
